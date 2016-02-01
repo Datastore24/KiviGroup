cb67a35cb494685b139dcf637d0fa4b9b50f8941
datastore24 <co@datastore24.ru>
2016-02-01 22:19:03 +0300

    Test

--- ITDolgopa/test/test/test/ViewController.m
+++ ITDolgopa/test/test/test/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    UIView * mainView;
+}
 
 @end
 
@@ -17,7 +19,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    mainView = [[UIView alloc] initWithFrame:CGRectMake(self.view.center.x, 40, 100, 40)];
+    mainView.center = self.view.center;
+    mainView.backgroundColor = [UIColor blackColor];
+    [self.view addSubview:mainView];
+    [self testMethod];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(testMethod) name:@"moveRight" object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(atestMethod) name:@"moveLeft" object:nil];
+
 }
+
+-(void)testMethod{
+
+    
+    [UIView animateWithDuration:1.0f animations:^{
+     
+        mainView.center = CGPointMake(self.view.center.x + 100, self.view.center.y);
+        
+    } completion:^(BOOL finished) {
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"moveLeft" object:nil];
+    }];
+
+}
+
+-(void)atestMethod{
+    [UIView animateWithDuration:1.0f animations:^{
+      mainView.center = CGPointMake(self.view.center.x - 100, self.view.center.y);
+    } completion:^(BOOL finished) {
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"moveRight" object:nil];
+    
+    }];
+    
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
