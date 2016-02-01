0a47de61c4822af3d11f2457d59d3eb8b1ee3e91
datastore24 <co@datastore24.ru>
2016-02-01 22:18:55 +0300

    Test

---
+++ ITDolgopa/test/test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Кирилл Ковыршин on 01.02.16.
+//  Copyright © 2016 datastore24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ITDolgopa/test/test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Кирилл Ковыршин on 01.02.16.
+//  Copyright © 2016 datastore24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
