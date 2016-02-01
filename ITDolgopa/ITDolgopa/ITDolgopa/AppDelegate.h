3cd4aa87171c5eca1a0c878273885bc66c884742
datastore24 <co@datastore24.ru>
2016-02-01 13:38:57 +0300

    New Project ITDolgopa

---
+++ ITDolgopa/ITDolgopa/ITDolgopa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ITDolgopa
+//
+//  Created by Кирилл Ковыршин on 01.02.16.
+//  Copyright © 2016 datastore24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
