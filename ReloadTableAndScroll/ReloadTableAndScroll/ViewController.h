4c415ccff5502263e35ab180a97b063f119288fe
datastore24 <co@datastore24.ru>
2016-08-13 23:13:24 +0300

    Reload

---
+++ ReloadTableAndScroll/ReloadTableAndScroll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReloadTableAndScroll
+//
+//  Created by Кирилл Ковыршин on 08.08.16.
+//  Copyright © 2016 datastore24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ReloadTableAndScroll/ReloadTableAndScroll/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReloadTableAndScroll
+//
+//  Created by Кирилл Ковыршин on 08.08.16.
+//  Copyright © 2016 datastore24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
