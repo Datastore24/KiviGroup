6f6520dd8071ba46e020fc7097804045667e46a5
Gyugo <mrgyugo@gmail.com>
2016-08-26 17:44:07 +0300

    Работа в вью покупки

---
+++ Sadovod/Sadovod/BuyView.h
@@ -0,0 +1,13 @@
+//
+//  BuyView.h
+//  Sadovod
+//
+//  Created by Виктор Мишустин on 26/08/16.
+//  Copyright © 2016 Виктор Мишустин. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BuyView : UIView
+
+@end
---
+++ Sadovod/Sadovod/BuyView.m
@@ -0,0 +1,13 @@
+//
+//  BuyView.m
+//  Sadovod
+//
+//  Created by Виктор Мишустин on 26/08/16.
+//  Copyright © 2016 Виктор Мишустин. All rights reserved.
+//
+
+#import "BuyView.h"
+
+@implementation BuyView
+
+@end
