f2127b97bfcf80b06c8f5b6b30a7a94e4bc6fb50
Gyugo <mrgyugo@gmail.com>
2016-09-05 13:58:54 +0300

    New classes

---
+++ Sadovod/Sadovod/FormalizationController.h
@@ -0,0 +1,13 @@
+//
+//  FormalizationController.h
+//  Sadovod
+//
+//  Created by Виктор Мишустин on 05/09/16.
+//  Copyright © 2016 Виктор Мишустин. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface FormalizationController : MainViewController
+
+@end
---
+++ Sadovod/Sadovod/FormalizationController.m
@@ -0,0 +1,13 @@
+//
+//  FormalizationController.m
+//  Sadovod
+//
+//  Created by Виктор Мишустин on 05/09/16.
+//  Copyright © 2016 Виктор Мишустин. All rights reserved.
+//
+
+#import "FormalizationController.h"
+
+@implementation FormalizationController
+
+@end
