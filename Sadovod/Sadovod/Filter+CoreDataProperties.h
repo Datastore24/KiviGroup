70c746f361507215a3ac2a47b4f073a0c2d58a75
datastore24 <co@datastore24.ru>
2016-09-06 10:30:04 +0300

    База данных фильтра

---
+++ Sadovod/Sadovod/Filter+CoreDataProperties.h
@@ -0,0 +1,26 @@
+//
+//  Filter+CoreDataProperties.h
+//  Sadovod
+//
+//  Created by Кирилл Ковыршин on 06.09.16.
+//  Copyright © 2016 Виктор Мишустин. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+#import "Filter.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Filter (CoreDataProperties)
+
+@property (nullable, nonatomic, retain) NSString *catID;
+@property (nullable, nonatomic, retain) NSString *min_cost;
+@property (nullable, nonatomic, retain) NSString *max_cost;
+@property (nullable, nonatomic, retain) NSString *sort;
+@property (nullable, nonatomic, retain) NSString *o;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Sadovod/Sadovod/FilterDbClass.h
@@ -0,0 +1,13 @@
+//
+//  FilterDB.h
+//  Sadovod
+//
+//  Created by Кирилл Ковыршин on 06.09.16.
+//  Copyright © 2016 Виктор Мишустин. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FilterDbClass : NSObject
+
+@end
