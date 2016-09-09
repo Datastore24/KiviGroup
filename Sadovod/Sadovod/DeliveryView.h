0e63f8ef792fe1530f56b397c2e858c02eb06d1d
Gyugo <mrgyugo@gmail.com>
2016-09-09 13:17:07 +0300

    Создание FAQ

---
+++ Sadovod/Sadovod/DeliveryView.h
@@ -0,0 +1,16 @@
+//
+//  DeliveryView.h
+//  Sadovod
+//
+//  Created by Виктор Мишустин on 09/09/16.
+//  Copyright © 2016 Виктор Мишустин. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DeliveryView : UIView
+
+- (instancetype)initWithView: (UIView*) view
+                     andData: (NSArray*) data;
+
+@end
