11cfa94ffa010bcb23ce0461c3ed35b08a25537a
Gyugo <mrgyugo@gmail.com>
2016-09-15 11:27:42 +0300

    Окончание верстки садовод

--- Sadovod/Sadovod/RegistrationController.m
+++ Sadovod/Sadovod/RegistrationController.m
@@ -28,7 +28,7 @@
     
 #pragma mark - View
     
-    RegistrationView * mainView = [[RegistrationView alloc] initWithView:self.view andData:nil];
+    RegistrationView * mainView = [[RegistrationView alloc] initWithView:self.view andData:[self createArray]];
     [self.view addSubview:mainView];
     
 }
@@ -38,6 +38,33 @@
 - (void) buttonBackAction {
     CatalogController * detail = [self.storyboard instantiateViewControllerWithIdentifier:@"CatalogController"];
     [self.navigationController pushViewController:detail animated:NO];
+}
+
+- (NSArray *) createArray {
+    NSMutableArray * arrayData = [[NSMutableArray alloc] init];
+    
+    NSArray * numberArray = [NSArray arrayWithObjects:@"843", @"236", @"346", @"824", @"148", @"534", nil];
+    NSArray * dateArray = [NSArray arrayWithObjects:
+                           @"13 Апреля в 18:34", @"24 Сентября в 10:34", @"10 Июля в 20:34",
+                           @"13 Мая в 13:50", @"30 Сентября в 10:00", @"13 Августа в 18:34", nil];
+    NSArray * arrayPrice = [NSArray arrayWithObjects:@"877", @"2470", @"3574", @"905", @"1746", @"780", nil];
+    NSArray * arrayStatus = [NSArray arrayWithObjects:
+                             @"В обработке", @"Собирается", @"Ожидает оплаты",
+                             @"Готов в отправке", @"Отправлен", @"Отменен", nil];
+    
+    for (int i = 0; i < numberArray.count; i++) {
+        NSDictionary * dict = [NSDictionary dictionaryWithObjectsAndKeys:
+                               [numberArray objectAtIndex:i], @"number",
+                               [dateArray objectAtIndex:i], @"date",
+                               [arrayPrice objectAtIndex:i], @"price",
+                               [arrayStatus objectAtIndex:i], @"status", nil];
+//        [arrayData addObject:dict];
+    
+    }
+    NSArray * mainArray = [NSArray arrayWithArray:arrayData];
+    
+    return mainArray;
+    
 }
 
 @end
--- Sadovod/Sadovod/RegistrationView.m
+++ Sadovod/Sadovod/RegistrationView.m
@@ -15,6 +15,13 @@
 #import "UIView+BorderView.h"
 #import "SingleTone.h"
 
+@interface RegistrationView () <UITableViewDelegate, UITableViewDataSource>
+
+@property (strong, nonatomic) NSArray * arrayData;
+@property (strong, nonatomic) UITableView * mainTable;
+
+@end
+
 @implementation RegistrationView
 
 - (instancetype)initWithView: (UIView*) view
@@ -23,6 +30,7 @@
     self = [super init];
     if (self) {
         self.frame = CGRectMake(0.f, 64.f, view.frame.size.width, view.frame.size.height - 64);
+        self.arrayData = data;
         
         if ([[[SingleTone sharedManager] typeMenu] isEqualToString:@"0"]) {
         
@@ -67,12 +75,149 @@
         buttonEntrance.titleLabel.font = [UIFont fontWithName:VM_FONT_REGULAR size:16];
         [viewCentre addSubview:buttonEntrance];
         } else {
-            
+            if (self.arrayData.count == 0) {
+                
+                UIImageView * imageGift = [[UIImageView alloc] initWithFrame:CGRectMake(self.frame.size.width / 2 - 50, self.frame.size.height / 2 - 140, 100, 100)];
+                imageGift.image = [UIImage imageNamed:@"gift.png"];
+                [self addSubview:imageGift];
+                
+                CustomLabels * labelGift = [[CustomLabels alloc] initLabelTableWithWidht:0.f andHeight:self.frame.size.height / 2 - 20 andSizeWidht:self.frame.size.width andSizeHeight:50
+                                                     andColor:@"d2d2d2" andText:@"Здесь будут отображаться \nВаши заказы"];
+                labelGift.font = [UIFont fontWithName:VM_FONT_REGULAR size:24];
+                labelGift.textAlignment = NSTextAlignmentCenter;
+                labelGift.numberOfLines = 2.f;
+                [self addSubview:labelGift];
+                
+                UIButton * buttonBack = [UIButton buttonWithType:UIButtonTypeSystem];
+                buttonBack.frame = CGRectMake(self.frame.size.width / 2 - 80, self.frame.size.height / 2 + 40, 160, 35);
+                buttonBack.backgroundColor = [UIColor hx_colorWithHexRGBAString:VM_COLOR_800];
+                buttonBack.layer.cornerRadius = 3.f;
+                [buttonBack setTitle:@"Вернуться на главную" forState:UIControlStateNormal];
+                [buttonBack setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+                buttonBack.titleLabel.font = [UIFont fontWithName:VM_FONT_REGULAR size:14];
+                [self addSubview:buttonBack];
+                
+                
+                
+            } else {
+                //Создание таблицы заказов----
+                self.mainTable = [[UITableView alloc] initWithFrame:CGRectMake(0.f, - 64.f, self.frame.size.width, self.frame.size.height + 64.f)];
+                //Убираем полосы разделяющие ячейки------------------------------
+                self.mainTable.separatorStyle = UITableViewCellSeparatorStyleNone;
+                self.mainTable.backgroundColor = nil;
+                self.mainTable.dataSource = self;
+                self.mainTable.delegate = self;
+                self.mainTable.showsVerticalScrollIndicator = NO;
+                //Очень полездное свойство, отключает дествие ячейки-------------
+                self.mainTable.allowsSelection = NO;
+                [self addSubview:self.mainTable];
+            } 
         }
         
     }
     
     return self;
 }
+
+
+#pragma mark - UITableViewDataSource
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return self.arrayData.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    static NSString *CellIdentifier = @"newFriendCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    for (UIView * view in cell.contentView.subviews) {
+        [view removeFromSuperview];
+    }
+    cell.backgroundColor = nil;
+    
+    NSDictionary * dict = [self.arrayData objectAtIndex:indexPath.row];
+    
+    
+    [cell.contentView addSubview:[self createCustomCellWithNumber:[dict objectForKey:@"number"]
+                                                          andDate:[dict objectForKey:@"date"]
+                                                         andPrice:[dict objectForKey:@"price"]
+                                                        andStatus:[dict objectForKey:@"status"]]];
+    
+    
+    
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+//Анимация нажатия ячейки--------------------------------------------------------------
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 60.f;
+}
+
+#pragma mark - CustomCell
+
+- (UIView*) createCustomCellWithNumber: (NSString*) number andDate: (NSString*) date
+                              andPrice: (NSString*) price andStatus: (NSString*) status {
+    UIView * mainViewCell = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, 60)];
+    
+    
+    [UIView borderViewWithHeight:59.5f andWight:0.f andView:mainViewCell andColor:@"5C5C5C"];
+    
+    CustomLabels * labelNumber = [[CustomLabels alloc] initLabelWithWidht:15.f andHeight:15 andColor:@"000000" andText:[NSString stringWithFormat:@"№%@", number] andTextSize:15 andLineSpacing:0.f fontName:VM_FONT_BOLD];
+    [mainViewCell addSubview:labelNumber];
+    
+    CustomLabels * labelDate = [[CustomLabels alloc] initLabelWithWidht:15.f andHeight:40 andColor:@"5C5C5C" andText:date andTextSize:13 andLineSpacing:0.f fontName:VM_FONT_REGULAR];
+    [mainViewCell addSubview:labelDate];
+    
+    CustomLabels * labelPrice = [[CustomLabels alloc] initLabelTableWithWidht:self.frame.size.width - 115.f andHeight:15 andSizeWidht:100.f andSizeHeight:15
+                                                                     andColor:VM_COLOR_800 andText:[NSString stringWithFormat:@"%@ руб", price]];
+    labelPrice.font = [UIFont fontWithName:VM_FONT_BOLD size:15];
+    labelPrice.textAlignment = NSTextAlignmentRight;
+    [mainViewCell addSubview:labelPrice];
+    
+    CustomLabels * labelStatus = [[CustomLabels alloc] initLabelTableWithWidht:self.frame.size.width - 115.f andHeight:40 andSizeWidht:100.f andSizeHeight:13
+                                                                     andColor:@"5C5C5C" andText:status];
+    labelStatus.font = [UIFont fontWithName:VM_FONT_REGULAR size:13];
+    labelStatus.textAlignment = NSTextAlignmentRight;
+    [mainViewCell addSubview:labelStatus];
+    
+    if ([status isEqualToString:@"Ожидает оплаты"]) {
+        mainViewCell.backgroundColor = [UIColor hx_colorWithHexRGBAString:@"FEFADE" alpha:0.7f];
+    } else if ([status isEqualToString:@"Готов в отправке"]) {
+        mainViewCell.backgroundColor = [UIColor hx_colorWithHexRGBAString:@"E6FEE5" alpha:0.7f];
+    } else if ([status isEqualToString:@"Отправлен"]) {
+        mainViewCell.backgroundColor = [UIColor hx_colorWithHexRGBAString:@"83FC7F" alpha:0.7f];
+    } else if ([status isEqualToString:@"Отменен"]) {
+        mainViewCell.backgroundColor = [UIColor hx_colorWithHexRGBAString:@"FD5A5A" alpha:0.3f];
+    }
+    
+
+    
+    
+    return mainViewCell;
+}
+
+
+
+
+
+
+
+
+
 
 @end
