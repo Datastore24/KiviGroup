3d8ffa2cfb8105437b682e166a8e4e809293fb4f
Gyugo <mrgyugo@gmail.com>
2016-09-21 11:14:05 +0300

    Ипправление ошибки делегата

--- Sadovod/Sadovod/AppDelegate.m
+++ Sadovod/Sadovod/AppDelegate.m
@@ -31,14 +31,10 @@
     
     //Тестовый синглтон для проверки авторизованного пользователя
     [[SingleTone sharedManager] setTypeMenu:@"0"]; //Если 0 то не авторизован
-<<<<<<< HEAD
     //Тестовые синглтоны для подсчета колличества выбранных товаров
     [[SingleTone sharedManager] setCountType:@"0"];
     [[SingleTone sharedManager] setPriceType:@"700"];
-=======
-   
 
->>>>>>> 111
     
     return YES;
 }
