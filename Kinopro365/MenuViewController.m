b89fe53fb3d34ae00944692044608406b85f9a1c
Gyugo <mrgyugo@gmail.com>
2016-11-17 18:02:11 +0300

    Rename

--- Kinopro365/MenuViewController.m
+++ Kinopro365/MenuViewController.m
@@ -6,14 +6,14 @@
 //  Copyright © 2016 kiviLab.com. All rights reserved.
 //
 
-#import "LeftSideViewController.h"
+#import "MenuViewController.h"
 #import "BackViewController.h"
 
-@interface LeftSideViewController ()
+@interface MenuViewController ()
 
 @end
 
-@implementation LeftSideViewController
+@implementation MenuViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -24,16 +24,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 - (IBAction)actionButtonBack:(id)sender {
     BackViewController * detail = [self.storyboard instantiateViewControllerWithIdentifier:@"BackViewController"];
