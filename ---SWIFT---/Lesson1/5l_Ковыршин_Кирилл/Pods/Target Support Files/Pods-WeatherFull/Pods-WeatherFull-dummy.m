27335b5edc57bbd22d1e4017e919e628f30c2656
datastore24 <co@datastore24.ru>
2016-12-12 13:36:38 +0300

    SWIFT

--- ---SWIFT---/Lesson1/5l_Ковыршин_Кирилл/Pods/Target Support Files/Pods-WeatherFull/Pods-WeatherFull-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WeatherFull : NSObject
-@end
-@implementation PodsDummy_Pods_WeatherFull
-@end
--- ---SWIFT---/Lesson1/5l_Ковыршин_Кирилл/Pods/Target Support Files/Pods-WeatherFull/Pods-WeatherFull-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_WeatherFullVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_WeatherFullVersionString[];
-
