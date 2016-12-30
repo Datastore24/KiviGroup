93d0759667dfa7554d7f5de91919cb853a0e69f8
Виктор Мишустин <mrgyugo@gmail.com>
2016-12-30 11:40:14 +0300

    6

---
+++ 2укауа/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  2укауа
+//
+//  Created by Виктор Мишустин on 29.12.16.
+//  Copyright © 2016 Viktor Mishustin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UITabBarController
+
+@end
