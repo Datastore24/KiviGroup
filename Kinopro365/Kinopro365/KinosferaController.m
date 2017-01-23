b6b2233b013ed5ed9c3025e4f21e348fb8a7384e
Виктор Мишустин <mrgyugo@gmail.com>
2017-01-23 16:58:25 +0300

    Исправление графический ошибок

---
+++ Kinopro365/Kinopro365/KinosferaController.m
@@ -0,0 +1,32 @@
+//
+//  KinosferaController.m
+//  Kinopro365
+//
+//  Created by Виктор Мишустин on 23.01.17.
+//  Copyright © 2017 kiviLab.com. All rights reserved.
+//
+
+#import "KinosferaController.h"
+
+@interface KinosferaController ()
+
+@end
+
+@implementation KinosferaController
+
+- (void) loadView {
+    [super loadView];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Kinopro365/Kinopro365/KinosferaModel.h
@@ -0,0 +1,13 @@
+//
+//  KinosferaModel.h
+//  Kinopro365
+//
+//  Created by Виктор Мишустин on 23.01.17.
+//  Copyright © 2017 kiviLab.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KinosferaModel : NSObject
+
+@end
