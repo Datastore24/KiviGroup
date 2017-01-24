0ac3553b1092133f07e6639fd069bfbba6a3344f
Виктор Мишустин <mrgyugo@gmail.com>
2017-01-24 15:03:08 +0300

    Отображение доп параметров

---
+++ Kinopro365/Kinopro365/AddParamsView.m
@@ -0,0 +1,15 @@
+//
+//  AddParamsView.m
+//  Kinopro365
+//
+//  Created by Виктор Мишустин on 24.01.17.
+//  Copyright © 2017 kiviLab.com. All rights reserved.
+//
+
+#import "AddParamsView.h"
+
+@implementation AddParamsView
+
+
+
+@end
