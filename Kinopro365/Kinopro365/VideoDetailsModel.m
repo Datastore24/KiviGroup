239bfd1c7a481c4a6efbb7ea9a06ead8280ba5c6
Кирилл Ковыршин <co@datastore24.ru>
2017-02-06 15:12:45 +0300

    Модель для фото и видео

--- Kinopro365/Kinopro365/VideoDetailsModel.m
+++ Kinopro365/Kinopro365/VideoDetailsModel.m
@@ -9,5 +9,31 @@
 #import "VideoDetailsModel.h"
 
 @implementation VideoDetailsModel
+@synthesize delegate;
+
+
+- (void) getVideoArrayWithOffset: (NSString *) offset andCount: (NSString *) count {
+    
+    APIManger * apiManager = [[APIManger alloc] init];
+    NSDictionary * params = [[NSDictionary alloc] initWithObjectsAndKeys:
+                             offset, @"offset",
+                             count, @"count",nil];
+    [apiManager getDataFromSeverWithMethod:@"video.getProfile" andParams:params andToken:[[SingleTone sharedManager] token] complitionBlock:^(id response) {
+        if([response objectForKey:@"error_code"]){
+            
+            NSLog(@"Ошибка сервера код: %@, сообщение: %@",[response objectForKey:@"error_code"],
+                  [response objectForKey:@"error_msg"]);
+            NSInteger errorCode = [[response objectForKey:@"error_code"] integerValue];
+        }else{
+            NSLog(@"RESPONSE VIDEO %@",response);
+            
+            NSDictionary *dictResponse = [response objectForKey:@"response"];
+            NSArray *itemsArray = [dictResponse objectForKey:@"items"];
+            [self.delegate loadVideo:itemsArray];
+        }
+        
+    }];
+}
+
 
 @end
