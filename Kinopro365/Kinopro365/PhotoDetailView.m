5c497eda8d441f458282c90b9ce22c487684f4e4
Виктор Мишустин <mrgyugo@gmail.com>
2017-02-07 12:54:53 +0300

    Photo View Details

--- Kinopro365/Kinopro365/PhotoDetailView.m
+++ Kinopro365/Kinopro365/PhotoDetailView.m
@@ -12,22 +12,46 @@
 @implementation PhotoDetailView
 
 
-- (instancetype)initWithImage: (UIImage*) image andFrame: (CGRect) frame
+- (instancetype)initWithImage: (NSString*) image andID: (NSString*) stringID andFrame: (CGRect) frame
 {
     self = [super init];
     if (self) {
         self.frame = frame;
+        
+        
+        NSData * imageData = [[NSData alloc] initWithContentsOfURL: [NSURL URLWithString: image]];
+        UIImage * image = [UIImage imageWithData: imageData];
+        
         CustomButton * buttonPhoto = [CustomButton buttonWithType:UIButtonTypeCustom];
         buttonPhoto.frame = self.bounds;
         [buttonPhoto setImage:image forState:UIControlStateNormal];
         buttonPhoto.isBool = NO;
         [buttonPhoto addTarget:self action:@selector(actionButtonPhoto:) forControlEvents:UIControlEventTouchUpInside];
+        buttonPhoto.backgroundColor = [UIColor blueColor];
         [self addSubview:buttonPhoto];
+        
+        self.stringID = stringID;
         
         self.imageViewDelete = [[UIImageView alloc] initWithFrame:CGRectMake(self.bounds.size.width - 30, self.bounds.size.height - 30, 25, 25)];
         self.imageViewDelete.image = [UIImage imageNamed:@"mark"];
         self.imageViewDelete.alpha = 0.f;
         [self addSubview:self.imageViewDelete];
+        
+    }
+    return self;
+}
+
+
+
+
+- (instancetype)initWithCustomFrame:(CGRect)frame
+{
+    self = [super init];
+    if (self) {
+        self.frame = frame;
+        
+        self.imageView = [[UIImageView alloc] initWithFrame:self.bounds];
+        [self addSubview:self.imageView];
         
     }
     return self;
