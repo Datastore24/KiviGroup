98a1266be7d19e318106ec72fe1ba8c30d4f01d0
Мишустин Сергеевич <mrgyugo@gmail.com>
2017-03-07 13:39:01 +0300

    Кастинги

---
+++ Kinopro365/Kinopro365/CastingDetailsController.h
@@ -0,0 +1,13 @@
+//
+//  CastingDetailsController.h
+//  Kinopro365
+//
+//  Created by Мишустин Сергеевич on 07.03.17.
+//  Copyright © 2017 kiviLab.com. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface CastingDetailsController : MainViewController
+
+@end
