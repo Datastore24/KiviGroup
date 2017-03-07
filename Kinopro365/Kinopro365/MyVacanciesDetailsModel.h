594ac75c35a0a9d8eac617b18e00dcc019a2160e
Кирилл Ковыршин <co@datastore24.ru>
2017-03-07 11:34:17 +0300

    Детали моих вакансий

---
+++ Kinopro365/Kinopro365/MyVacanciesDetailsModel.h
@@ -0,0 +1,13 @@
+//
+//  MyVacanciesDetailsModel.h
+//  Kinopro365
+//
+//  Created by Кирилл Ковыршин on 07.03.17.
+//  Copyright © 2017 kiviLab.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyVacanciesDetailsModel : NSObject
+
+@end
---
+++ Kinopro365/Kinopro365/MyVacanciesDetailsModel.m
@@ -0,0 +1,13 @@
+//
+//  MyVacanciesDetailsModel.m
+//  Kinopro365
+//
+//  Created by Кирилл Ковыршин on 07.03.17.
+//  Copyright © 2017 kiviLab.com. All rights reserved.
+//
+
+#import "MyVacanciesDetailsModel.h"
+
+@implementation MyVacanciesDetailsModel
+
+@end
