7b7fbcbbcb7de8e4e45ed55d2f58ec7c30f75d60
Кирилл Ковыршин <co@datastore24.ru>
2017-03-13 15:54:15 +0300

    Добавление

---
+++ Kinopro365/Kinopro365/AddCastingModel.h
@@ -0,0 +1,13 @@
+//
+//  AddCastingModel.h
+//  Kinopro365
+//
+//  Created by Кирилл Ковыршин on 13.03.17.
+//  Copyright © 2017 kiviLab.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AddCastingModel : NSObject
+
+@end
---
+++ Kinopro365/Kinopro365/AddCastingModel.m
@@ -0,0 +1,13 @@
+//
+//  AddCastingModel.m
+//  Kinopro365
+//
+//  Created by Кирилл Ковыршин on 13.03.17.
+//  Copyright © 2017 kiviLab.com. All rights reserved.
+//
+
+#import "AddCastingModel.h"
+
+@implementation AddCastingModel
+
+@end
